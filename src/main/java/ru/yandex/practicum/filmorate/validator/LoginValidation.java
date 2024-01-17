8d5a10d8bf42264924458b670c5c27829ae74c0b
Nikolai Degtiarev <NikAndDeg@yandex.ru>
2024-01-17 19:08:14 +0500

    Убрать неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/validator/LoginValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/LoginValidation.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.validator;
 
 import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 
 import java.lang.annotation.Documented;
--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidation.java
@@ -1,15 +1,12 @@
 package ru.yandex.practicum.filmorate.validator;
 
 import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.time.LocalDate;
 
 import static java.lang.annotation.ElementType.*;
 
