c7924f767229331543c099b994c9fdc50b84d4e1
Nikolai Degtiarev <NikAndDeg@yandex.ru>
2024-02-25 16:52:29 +0500

    checkstyle6

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/user/UserFriendsDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/user/UserFriendsDao.java
@@ -6,10 +6,16 @@ import java.util.Set;
 
 public interface UserFriendsDao {
 	void save(int userId, Set<Integer> friends);
+
 	Set<Integer> get(int userId);
+
 	Map<Integer, Set<Integer>> get(List<Integer> usersId);
+
 	Map<Integer, Set<Integer>> getAll();
+
 	void add(int userId, int friendId);
+
 	void remove(int userId, int friendId);
+
 	List<Integer> getMutualFriend(int userId, int otherUserId);
 }
